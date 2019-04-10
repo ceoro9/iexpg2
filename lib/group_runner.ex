71d0c20c35787574a7b803090e7abccd898fae88
ceoro9 <ceoro9@protonmail.com>
2019-04-10 23:59:06 +0300

    Adding processes to group

--- lib/group_runner.ex
+++ lib/group_runner.ex
@@ -4,12 +4,16 @@ defmodule Pg2.GroupRunner do
   @process_name_suffix "instance_"
 
   def start_link([group_name | _]) do
-    GenServer.start_link(__MODULE__, [], name: get_process_name(group_name))
+    GenServer.start_link(__MODULE__, [group_name], name: get_process_name(group_name))
   end
 
   @impl true
-  def init(_) do
-    {:ok, %{}}
+  def init(group_name) do
+    {:ok,
+     %{
+       group_name: group_name,
+       processes: []
+     }}
   end
 
   defp get_process_name(group_name) do
@@ -19,20 +23,62 @@ defmodule Pg2.GroupRunner do
   @impl true
   def handle_call({:call, message}, _from, state) do
     # TODO: use task to make the job and wait
-    do_handle_call(message)
+    do_handle_call(message, state)
     {:reply, :ok, state}
+  end
+
+  @impl true
+  def handle_call({:add_process, pid}, _from, state) do
+    case Process.alive?(pid) do
+      true ->
+        ref = Process.monitor(pid)
+
+        if handle_process_in_group?(pid, state) do
+          {:reply, {:error, "Process is already in group"}, state}
+        else
+          {:reply, :ok, %{state | processes: [{ref, pid} | state.processes]}}
+        end
+
+      false ->
+        {:reply, {:error, "Process is dead"}, state}
+    end
+  end
+
+  def handle_call(:list_processes, _from, state) do
+    {:reply, Enum.map(state.processes, fn {_, pid} -> pid end), state}
+  end
+
+  def handle_call({:in_group?, searched_process_pid}, _from, state) do
+    {:reply, process_in_group?(searched_process_pid, state), state}
+  end
+
+  defp handle_process_in_group?(searched_process_pid, state) do
+    state.processes
+    |> Enum.map(fn {_, pid} -> pid end)
+    |> Enum.member?(searched_process_pid)
   end
 
   @impl true
   def handle_cast({:call, message}, state) do
     # TODO: use task to make the job and do not wait
-    do_handle_call(message)
+    do_handle_call(message, state)
     {:noreply, state}
   end
 
-  defp do_handle_call(message) do
-    # TODO:
+  @impl true
+  def handle_info({:DOWN, monitor_ref, :process, _from_pid, _exit_reason}, state) do
+    IO.puts("exit process")
+
+    {:noreply,
+     %{state | processes: Enum.filter(state.processes, fn {ref, _} -> ref != monitor_ref end)}}
+  end
+
+  defp do_handle_call(message, state) do
     # iterate over running processes and put message to their mailbox
+    state.processes
+    |> Enum.map(fn {_, pid} -> pid end)
+    |> Enum.each(fn pid -> send(pid, message) end)
+
     IO.puts("got new message: ")
     IO.inspect(message)
     {:ok}
@@ -45,5 +91,21 @@ defmodule Pg2.GroupRunner do
       get_process_name(group_name),
       {:call, message}
     )
+  end
+
+  def add_process(group_name, process_pid) do
+    GenServer.call(get_process_name(group_name), {:add_process, process_pid})
+  end
+
+  def list_current_running_processes(group_name) do
+    GenServer.call(get_process_name(group_name), :list_processes)
+  end
+
+  def group_exists?(group_name) do
+    get_process_name(group_name) |> Process.alive?()
+  end
+
+  def process_in_group?(group_name, pid) do
+    GenServer.call(get_process_name(group_name), {:in_group?, pid})
   end
 end
